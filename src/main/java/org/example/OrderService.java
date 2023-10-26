4f0b09a14194b3ba0b69236778970172669eaac1
Xtraa777 <mazino0588@gmail.com>
2023-10-26 11:14:16 +0900

    요청사항 입력

--- src/main/java/org/example/OrderService.java
+++ src/main/java/org/example/OrderService.java
@@ -12,8 +12,8 @@ public class OrderService {
         System.out.println("아래와 같이 주문 하시겠습니까?");
         System.out.println("[ Orders ]");
 
-        List<Product> cart = order.getCart();
-        double total = 0;
+        List<Product> cart  = order.getCart();
+        double        total = 0;
         for (int i = 0; i < cart.size(); i++) {
             Product p = cart.get(i);
             System.out.println(String.format("%-15s | W %5.0f | %s", p.getName(), p.getPrice(), p.getDescription()));
@@ -27,19 +27,27 @@ public class OrderService {
 
         int choice = scanner.nextInt();
 
-        if (choice==1) {
+        if (choice == 1) {
             completeOrder(waitOrder, order);
         }
     }
 
-
     public static void completeOrder(List<Order> waitOrder, Order order) {
+        System.out.println("요구사항을 입력해주세요(최대 20자)");
+        Scanner scanner = new Scanner(System.in);
+        String  message = scanner.nextLine();
+
+        if (message.length() > 20) {
+            System.out.println("입력한 요구사항이 20자를 초과했습니다. 처음 20자까지만 저장됩니다.");
+            message = message.substring(0, 20);
+        }
+
         System.out.println("__________________________________\n");
 
         System.out.println("주문이 완료되었습니다!");
-        Order o = new Order();
+        Order         o    = new Order();
         List<Product> cart = new ArrayList<>(order.getCart());
-        o.setCart(cart);
+        o.setCart(cart, message);
         waitOrder.add(o);
         System.out.println(String.format("대기번호는 [ %s ] 번 입니다.", waitOrder.size()));
         System.out.println("(3초후 메뉴판으로 돌아갑니다.)");
@@ -58,7 +66,7 @@ public class OrderService {
         System.out.println("주문을 취소할까요? (1. 확인 / 2. 취소)");
         int choice = sc.nextInt();
 
-        if (choice==1) {
+        if (choice == 1) {
             order.clearCart();
             System.out.println("__________________________________\n");
             System.out.println("주문이 취소되었습니다.");
